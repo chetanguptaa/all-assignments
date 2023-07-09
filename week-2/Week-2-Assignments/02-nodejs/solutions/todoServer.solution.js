4309df5f92a1243a3444de695fcedb84e32fd4ac
chetan gupta <chetangupta123raj@gmail.com>
2023-07-09 18:41:45 +0530

    completed week-4 easy

--- week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.js
+++ week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.js
@@ -41,12 +41,21 @@
  */
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
+
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
-let todos = [];
+let todos = [
+  {
+    id: 1,
+    title: "her",
+    description: "nope",
+  }
+];
 
 app.get('/todos', (req, res) => {
   res.json(todos);
@@ -96,5 +105,9 @@ app.delete('/todos/:id', (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send();
 });
+
+app.listen(3000, () => {
+  console.log("app is listening on port 3000");
+})
 
 module.exports = app;
--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/todos'); 
+      setTodos(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const createTodo = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/todos', {
+        title: newTodo,
+        description: '' 
+      });
+      setTodos([...todos, response.data]);
+      setNewTodo('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/todos/${id}`); 
+      setTodos(todos.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <h1>Easy Todo App</h1>
-        <input type="text" />
-      </div>
-    </>
-  )
-}
+    <div>
+      <h1>TODO App</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={createTodo}>Add Todo</button>
 
-function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <h3>{todo.title}</h3>
+            <p>{todo.description}</p>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
+  )
 }
 
 export default App
